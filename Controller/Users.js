4df4071e82b9ce755aebd17be426a5bded82bfd5
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2022-12-13 02:08:18 +0500

    changes

--- Controller/Users.js
+++ Controller/Users.js
@@ -1,15 +1,37 @@
 const UserModel = require('../Models/Users');
+const CryptoJS = require("crypto-js");
 
-const CreateNewUser = async (req,res,next) => {
-    const user = new UserModel({
-        name : req.body.name,
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password,
-        verifiedUser : req.body.verifiedUser
-    });
-    const Registered_Users = await user.save();
-    console.log(Registered_Users)
+const CreateNewUser = async (req, res, next) => {
+    try {
+        const user = new UserModel({
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+            password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
+            verifiedUser: req.body.verifiedUser
+        });
+
+        if (req.body.password.length > 7) {
+            const Registered_Users = await user.save();
+            res.send({
+                message: "User Created Successfully",
+                status: 201,
+                data: Registered_Users
+            })
+            return Registered_Users;
+
+        } else {
+            res.send({
+                message: "Password Must be 8 charactor",
+                status: 500
+            })
+        }
+    } catch (err) {
+        res.send({
+            message: "User Not Found",
+            status: 404
+        })
+    }
 }
 
 
