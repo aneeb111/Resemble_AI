e7752d14fa50bc9f2e67704674952980ed6f5a7b
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2022-12-22 23:20:51 +0500

    changes

--- Controller/Clip.js
+++ Controller/Clip.js
@@ -1,7 +1,7 @@
 const { Resemble } = require('@resemble/node');
 const UserModel = require('../Models/Users');
 
-const Create_New_Clip = async (req,res) => {
+const Create_New_User_Clip = async (req,res) => {
     try{
         var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
         Resemble.setApiKey(process.env.RESEMBLE_AI_KEY);
@@ -12,7 +12,33 @@ const Create_New_Clip = async (req,res) => {
         const name = pro.item.name 
         
         const data = await Resemble.v2.clips.createAsync(Project_id,{
-            // body : `Hi` +" "+User.username +" "+ req?.body?.body +" "+ name ,
+            title : req.body.title,
+            body : `Hi` +" "+User.username +" "+ req?.body?.body +" "+ name,
+            voice_uuid : req?.body?.voice_uuid ,
+            callback_uri : fullUrl
+            
+        })
+        res.send({
+            message:data.message,
+            status:201,
+            data: data.item
+        })
+    }catch(err){
+        res.send({
+            message:data.message,
+            status:404
+        }) 
+    }
+
+    
+}
+
+const Create_New_Clip = async (req,res) => {
+    try{
+        var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        Resemble.setApiKey(process.env.RESEMBLE_AI_KEY);
+        const Project_id = req.params.uuid;
+        const data = await Resemble.v2.clips.createAsync(Project_id,{
             title : req.body.title,
             body : req?.body?.body,
             voice_uuid : req?.body?.voice_uuid ,
@@ -119,6 +145,7 @@ const Update_Specific_Clip = async (req,res) => {
 }
 
 module.exports = {
+    Create_New_User_Clip,
     Create_New_Clip,
     Get_All_Clips,
     Get_Specific_Clip,
--- Routes/Clip.js
+++ Routes/Clip.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const { 
 
+    Create_New_User_Clip,
     Create_New_Clip,
     Get_All_Clips,
     Get_Specific_Clip,
@@ -11,7 +12,8 @@ const {
 } = require('../Controller/Clip')
 
 
-router.post('/User_id/:uid/Project_id/:uuid/clips' , Create_New_Clip );
+router.post('/User_id/:uid/Project_id/:uuid/clips' , Create_New_User_Clip );
+router.post('/Project_id/:uuid/createclips' , Create_New_Clip );
 router.get('/Project_id/:uuid/Getall' , Get_All_Clips );
 router.get('/Project_id/:p_id/Clip_id/:c_id' , Get_Specific_Clip );
 router.delete('/Project_id/:p_id/Clip_id/:c_id' , Delete_Clip );
